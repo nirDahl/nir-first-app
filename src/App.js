dd7ab243f6c16b8be4735550b298d336b7d3b3f5
Nirjala Dahal <nirjala.dahal@gmail.com>
2018-12-04 20:21:50 -0500

    adding only one of two updated files

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ class App extends Component {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn and Earn
+            Learn and Earn and earn too much.
           </a>
         </header>
       </div>
